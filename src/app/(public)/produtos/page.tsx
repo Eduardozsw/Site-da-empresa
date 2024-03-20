720255a155ab11f91192c30b36d5cbdfd8c8fa7e
Eduardo <dudusoutomaior2006@gmail.com>
2024-03-20 14:51:21 -0300

    version 1.7.41 content update

--- src/app/(public)/produtos/page.tsx
+++ src/app/(public)/produtos/page.tsx
@@ -2,10 +2,10 @@ export default function Produtos() {
     return <>
         <main className="pt-4 flex flex-col justify-center items-center">
             <title>Produtos</title>
-            <h1 className="text-4xl font-bold pb-4">produtos</h1>
-            <section className="flex flex-wrap px-80 justify-center items-center gap-6">
+            <h1 className="text-4xl font-bold pb-6">produtos</h1>
+            <section className="flex flex-wrap text-center gap-6">
                 <button>
-                    <div className="border-2 rounded-lg">
+                    <div className="box border-2 rounded-lg">
                         <div className="flex flex-col justify-center items-center p-10">
                             <h1 className="font-bold text-2xl">Design</h1>
                             <div className="flex flex-wrap pt-5">
@@ -15,21 +15,53 @@ export default function Produtos() {
                     </div>
                 </button>
                 <button>
-                    <div className="border-2 rounded-lg">
+                    <div className="box border-2 rounded-lg">
                         <div className="flex flex-col justify-center items-center p-10">
-                            <h1 className="font-bold text-2xl">Design</h1>
+                            <h1 className="font-bold text-2xl">Front-end</h1>
                             <div className="flex flex-wrap pt-5">
-                                <h1 className="font-semibold">Dê uma nova cara a sua marca</h1>
+                                <h1 className="font-semibold">Deixe que um dos nossos profissionais<br/> te ajude a fazer o visual do seu site</h1>
                             </div>
                         </div>
                     </div>
                 </button>
                 <button>
-                    <div className="border-2 rounded-lg">
+                    <div className="box border-2 rounded-lg">
                         <div className="flex flex-col justify-center items-center p-10">
-                            <h1 className="font-bold text-2xl">Design</h1>
+                            <h1 className="font-bold text-2xl">Back-end</h1>
                             <div className="flex flex-wrap pt-5">
-                                <h1 className="font-semibold">Dê uma nova cara a sua marca</h1>
+                                <h1 className="font-semibold">Torne funcional a sua landing page<br/> com sistemas de contas e perfis</h1>
+                            </div>
+                        </div>
+                    </div>
+                </button>
+            </section>
+            <section className="flex flex-wrap gap-6 pt-6">
+                <button>
+                    <div className="box border-2 rounded-lg">
+                        <div className="flex flex-col justify-center items-center p-10">
+                            <h1 className="font-bold text-2xl">FullStack</h1>
+                            <div className="flex flex-wrap pt-5">
+                                <h1 className="font-semibold">Crie um site para seu negócio e aumente sua visibilidade</h1>
+                            </div>
+                        </div>
+                    </div>
+                </button>
+                <button>
+                    <div className="box border-2 rounded-lg">
+                        <div className="flex flex-col justify-center items-center p-10">
+                            <h1 className="font-bold text-2xl">App Nativo</h1>
+                            <div className="flex flex-wrap pt-5">
+                                <h1 className="font-semibold">Crie um aplicativo para seu negócio em um único sistema de celular</h1>
+                            </div>
+                        </div>
+                    </div>
+                </button>
+                <button>
+                    <div className="box border-2 rounded-lg">
+                        <div className="flex flex-col justify-center items-center p-10">
+                            <h1 className="font-bold text-2xl">App</h1>
+                            <div className="flex flex-wrap pt-5">
+                                <h1 className="font-semibold">Crie um aplicativo para seu negócio para que qualquer um veja sua marca</h1>
                             </div>
                         </div>
                     </div>
--- src/app/(public)/tecnologias2/page.tsx
+++ src/app/(public)/tecnologias2/page.tsx
@@ -3,7 +3,7 @@ export default function Tecnologias() {
         <main className="flex flex-col justify-center items-center">
             <h1 className=" text-black text-4xl font-bold pb-4">Tecnologias</h1>
             <section className="flex flex-wrap px-12">
-                <div className="border-2 rounded-lg flex flex-col justify-center items-center py-2 px-6 mx-12">
+                <div className="border-2 rounded-lg flex flex-col justify-center items-center py-4 px-12 mx-12">
                     <h1 className="font-bold text-2xl pt-4">Front-end</h1>
                     <div className="flex flex-col text-center pt-4 pb-4">
                         <p className="tec">React</p>
@@ -12,22 +12,22 @@ export default function Tecnologias() {
                         <p className="tec">Next</p>
                     </div>
                 </div>
-                <div className="border-2 rounded-lg flex flex-col justify-center items-center py-2 px-6 mx-12">
+                <div className="border-2 rounded-lg flex flex-col justify-center items-center py-4 px-12 mx-12">
                     <h1 className="font-bold text-2xl pt-4">Back-end</h1>
                     <div className="flex flex-col text-center pt-4 pb-4">
-                        <p className="tec">React</p>
-                        <p className="tec">HTML5</p>
-                        <p className="tec">Javascript</p>
-                        <p className="tec">Next</p>
+                        <p className="tec">Java</p>
+                        <p className="tec">PHP</p>
+                        <p className="tec">Python</p>
+                        <p className="tec">Node.js</p>
                     </div>
                 </div>
-                <div className="border-2 rounded-lg flex flex-col justify-center items-center py-2 px-6 mx-12">
+                <div className="border-2 rounded-lg flex flex-col justify-center items-center py-4 px-12 mx-12">
                     <h1 className="font-bold text-2xl pt-4">UX-Design</h1>
                     <div className="flex flex-col text-center pt-4 pb-4">
-                        <p className="tec">React</p>
-                        <p className="tec">HTML5</p>
-                        <p className="tec">Javascript</p>
-                        <p className="tec">Next</p>
+                        <p className="tec">Figma</p>
+                        <p className="tec">Adobe XD</p>
+                        <p className="tec">Sketch</p>
+                        <p className="tec">InVision</p>
                     </div>
                 </div>
             </section>
